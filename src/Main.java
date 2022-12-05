c3cf6a66834fd157f96948f429073bb4aafb916a
Your Name <you@example.com>
2022-12-05 14:33:50 +0700

    module2

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import java.util.Date;//thư viên
+public class Main {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("Time is " + now);
+    }
+}
